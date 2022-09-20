b19781dae2154e078c728513f4f27927a2592956
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-09-20 14:24:59 +0530

    :sparkles: Added Submission page
    Added submitted page and made the page submitted.tsx

---
+++ pages/submitted.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SubmittedForm from '../Components/SubmittedForm'
+
+function submitted() {
+  return (
+    <div className='flex justify-center mt-52'>
+        <SubmittedForm/>
+    </div>
+  )
+}
+
+export default submitted
