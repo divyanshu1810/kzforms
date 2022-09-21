ef2838f7e0a0acf7cb229820dc435bd46c46695c
Divyanshu Yadav <divyanshu.1810@gmail.com>
2022-09-21 18:40:00 +0530

    :bulb: Added comments
    Added comments and UI fixes

--- Components/Form.tsx
+++ Components/Form.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios';
 function Form() {
+    // Hooks to handle the state of the valid entries, userData and formErrors
     const [valid,setValid] = React.useState(false);
     const [formError,setFormError] = React.useState('')
     const [formData, setFormData] = React.useState(
@@ -11,6 +12,7 @@ function Form() {
             name:""
         }      
     ) 
+    // function to check the validity of the github url entered
     function validGithub(String:string){
         let url;
         try {
@@ -20,6 +22,7 @@ function Form() {
         }
          return url.protocol === "http:" || url.protocol === "https:";
     }
+    // function to check the validity of the Registration Number entered
     function validRegister(String:string){
         const initial = String.substring(0,2);
         const rest = String.substring(2);
@@ -29,14 +32,17 @@ function Form() {
         }
         return false;
     }
+    // function to check the validity of the email entered
     function validEmail(String:string){
        return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(String))
     }
+    // function to check the validity of the name entered
     function validName(String:string){
         const regName = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
         const name = String;
         return regName.test(name)
     }
+    // fucntion to handle the input changes happening  
     function handleChange(event:any){
         const {name,value} = event.target
         event.persist();
@@ -47,18 +53,21 @@ function Form() {
             }
         })
     }
+    // function to run when the user clicks on submit button
     function handleSubmit(event:any){
         event.preventDefault()
         if(validName(formData.name) && validGithub(formData.github) && validEmail(formData.email) && validRegister(formData.registration))
         {
+            // axios call to post the data to a dummy api (formbackend used here)
             axios.post(`${process.env.NEXT_PUBLIC_URL}`, {
                 registration: formData.registration,
                 github: formData.github,
                 email: formData.email,
                 name: formData.name,
-              })
-              .then(function (response) {
+            })
+            .then(function (response) {
                 console.table(formData);
+                // console.log(response)
                 setValid(!valid);
             })
             .catch(function (error) {
@@ -68,7 +77,22 @@ function Form() {
         else{
             if(formData.name.length==0 || formData.github.length==0 || formData.email.length==0 || formData.registration.length==0)
             {
-                setFormError('Please fill all the details properly')
+                if(formData.registration.length==0)
+                {
+                    setFormError('Registration Number can\'t be empty');
+                }
+                else if(formData.email.length==0)
+                {
+                    setFormError('Email can\'t be empty');
+                }
+                else if(formData.github.length==0)
+                {
+                    setFormError('Github can\'t be empty');
+                }
+                else
+                {
+                    setFormError('Name can\'t be empty')
+                }
             }
             else if(!validEmail(formData.email))
             {
@@ -90,8 +114,8 @@ function Form() {
     return(
         <>
         {!valid && <div className='lg:w-1/4 md:w-2/4 fold md:h-auto items-center bg-white w-auto p-4 py-4 rounded-xl shadow-xl '>
-            <div className='flex justify-center mx-4 mb-4  md:mb-0'>
-            <div className='text-xl font-semibold w-5/6 text-center md:pt-4 md:pb-10 text-gray-600'>Tell us about Yourself lad! 🥰</div>
+            <div className='flex justify-center mx-4 md:mb-4 '>
+            <div className='text-xl font-semibold w-5/6 text-center  md:pt-4 border-blue-500 border-b-2 text-gray-600 pad'>Tell us about Yourself lad! 🥰</div>
             </div>
         {/* bg-white bg-opacity-20 backdrop-blur-lg drop-shadow-lg */}
             <form name='myForm' onSubmit={handleSubmit} id="myForm" className=" flex flex-col  items-center">
@@ -127,10 +151,10 @@ function Form() {
             name="name"   
             value={formData.name} 
             />
-            {formError && <div className='mt-6 -mb-4  text-gray-600 border-b-2 border-red-600 pb-2 font-bold px-4 py-1'>Error : {formError}</div>}
+            {formError && <div className='mt-6 -mb-4  text-gray-600 border-b-4 border-red-600 pb-2 font-bold px-4 py-1'>Error : {formError}</div>}
             <input className='mt-10 bg-blue-900 p-2 w-4/6 mb-6 hover:bg-blue-600 text-center duration-150 cursor-pointer text-white rounded-lg ' 
             type="submit" 
-            value="Send Information">
+            value="Submit">
             </input>
         </form>
         </div>}
